bf2a18913f667d4967c6b9c8cf384979a4fb1d12
Xinyi Ding <dxywill@gmail.com>
2016-09-07 10:34:11 -0500

    Add viewcontroller

---
+++ PhotoPicker/ImageCollectionController.swift
@@ -0,0 +1,15 @@
+//
+//  ImageCollectionController.swift
+//  PhotoPicker
+//
+//  Created by Xinyi Ding on 9/7/16.
+//  Copyright © 2016 Xinyi. All rights reserved.
+//
+
+import Foundation
+import UIKit
+
+class ImageCollectionController: UICollectionViewController {
+    
+    
+}
---
+++ PhotoPicker/ImageDetailController.swift
@@ -0,0 +1,15 @@
+//
+//  ImageDetailController.swift
+//  PhotoPicker
+//
+//  Created by Xinyi Ding on 9/7/16.
+//  Copyright © 2016 Xinyi. All rights reserved.
+//
+
+import Foundation
+import UIKit
+
+class ImageDetailController: UIViewController {
+    
+    
+}
